d665bc87b3592ead42bccf4b568e7bcfcb2a0e8c
Sanghyu Lee <hyouoo@gmail.com>
2023-11-14 01:18:40 +0900

    feat: s3-client

---
+++ src/main/java/com/sparta/lv5/common/aws/MyS3Client.java
@@ -0,0 +1,41 @@
+package com.sparta.lv5.common.aws;
+
+import io.awspring.cloud.s3.ObjectMetadata;
+import io.awspring.cloud.s3.S3Template;
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+import org.springframework.util.StreamUtils;
+import software.amazon.awssdk.core.ResponseInputStream;
+import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.model.GetObjectResponse;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+import java.nio.charset.StandardCharsets;
+import java.time.Duration;
+
+@Component
+@RequiredArgsConstructor
+public class MyS3Client {
+
+    private static final String BUCKET = "mylv5bucket/images";
+    private final S3Client s3Client;
+    private final S3Template s3Template;
+
+    void readFile() throws IOException {
+        ResponseInputStream<GetObjectResponse> response = s3Client.getObject(
+                request -> request.bucket(BUCKET).key("a.txt")
+        );
+        String fileContent = StreamUtils.copyToString(response, StandardCharsets.UTF_8);
+        System.out.println(fileContent);
+    }
+
+    void uploadFile(HttpServletRequest request) throws IOException {
+        InputStream is = request.getInputStream();
+        s3Template.upload(BUCKET, "a.txt", is, ObjectMetadata.builder().contentType("text/plain").build());
+
+        URL signedGetUrl = s3Template.createSignedGetURL(BUCKET, "a.txt", Duration.ofMinutes(5));
+    }
+}
