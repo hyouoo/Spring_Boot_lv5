b1674e05f74d5a843d3e3c0f7a654eebdd3b4f7b
Sanghyu Lee <hyouoo@gmail.com>
2023-11-16 01:38:38 +0900

    fix: image delete bug

--- src/main/java/com/sparta/lv5/products/ImageService.java
+++ src/main/java/com/sparta/lv5/products/ImageService.java
@@ -8,6 +8,7 @@ import io.awspring.cloud.s3.S3Template;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
@@ -29,6 +30,7 @@ public class ImageService {
     @Value("${spring.cloud.aws.s3.region}")
     private String region;
 
+    @Transactional
     public ImageResponseDto addImage(Integer productId, MultipartFile file) throws IOException {
         Product product = productRepository.findById(productId).orElseThrow(() ->
                 new NullPointerException("해당 상품이 존재하지 않습니다."));
@@ -47,14 +49,20 @@ public class ImageService {
         return new ImageResponseDto(newimage);
     }
 
-
+    @Transactional
     public void deleteImage(Integer imageId) {
         Image image = imageRepository.findById(Long.valueOf(imageId)).orElseThrow(
                 () -> new NullPointerException("해당 이미지가 없습니다.")
         );
-        Pattern pattern = Pattern.compile("(images/([^/]+))");
+        Pattern pattern = Pattern.compile("com/(.*\\S)");
         Matcher matcher = pattern.matcher(image.getSrcurl());
-        String key = matcher.group(1);
+
+        String key;
+        if (matcher.find()) {
+            key = matcher.group(1);
+        } else {
+            throw new IllegalArgumentException("일치하는 패턴이 없습니다.");
+        }
         imageRepository.delete(image);
         s3Template.deleteObject(bucket, key);
     }
